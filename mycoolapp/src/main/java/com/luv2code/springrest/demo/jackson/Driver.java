7ac7e839558935fcef7367301a1689fd3fdb6f7f
yanmin <ymchan.dev@gmail.com>
2023-02-11 20:26:46 +0800

    sprint rest - json jackson demo - processing json

---
+++ mycoolapp/src/main/java/com/luv2code/springrest/demo/jackson/Driver.java
@@ -0,0 +1,27 @@
+package com.luv2code.springrest.demo.jackson;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+
+public class Driver {
+    public static void main(String[] args) {
+
+        try {
+            // create object mapper
+            ObjectMapper mapper = new ObjectMapper();
+
+            // read JSON file and map/convert to Java POJO:
+            // data/sample-lite.json
+            Student theStudent = mapper.readValue(new File("mycoolapp/data/sample-lite.json"), Student.class);
+
+            //print first name and last name
+            System.out.println("First name = " + theStudent.getFirstName());
+            System.out.println("Last name = " + theStudent.getLastName());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
